8d09b39a63965dc5757db608cd567267d178d10c
ep-loloh <pereira.eloara@escola.pr.gov.br>
2025-11-25 20:24:57 -0300

    Add files via upload
    o

---
+++ main.js
@@ -0,0 +1,13 @@
+function tocaSom(idElementoaudio){
+ const elemento=Document.QuerySelector(idElementoaudio)
+ if(elemento && elemento.localName=== 'audio'){
+       elemento.play ( );
+ }else{
+    console.log('Elemento não encontrado invalido');
+   } 
+   }
+   const listaDeTeclas=document.
+ 
+
+ 
+
