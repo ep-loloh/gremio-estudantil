cc46291cc57d5cc83d7dfea942acf0e7aa42d02f
ep-loloh <pereira.eloara@escola.pr.gov.br>
2025-12-02 20:16:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,33 @@ function tocaSom(idElementoaudio){
  if(elemento && elemento.localName=== 'audio'){
        elemento.play ( );
  }else{
-    console.log('Elemento não encontrado invalido');
+    console.log("Elemento não encontrado inválido");
    } 
    }
-   const listaDeTeclas=document.
+   const listaDeTeclas=document.querySelectorAll('.tecla')
+
+
+
+
+   for(letcontador=0,contador<listaDeTeclas.length,contador++)
+   const tecla=listaDeTeclas[contador];
+   const efeito=tecla.classlist[1];
+   const idaudio= #som_${efeito};
+   tecla.onclick=function(){
+   tocaSom(idaudio);
+   }
+
+   tecla.onkeydown=function(evento){
+   if(evento.code==='Spale')
+   tecla.classlist.List.add('ativa');
+  }
+} 
+   tecla.onkeydown=function(elemento){
+    if(evento.code==='enter')
+      tecla.classlist.add('ativa');
  
+   }
 
  
+
 
